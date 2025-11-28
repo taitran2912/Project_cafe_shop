95ad12bb806708bee7f9225d4c12f2520eddeaa4
taitran2912 <trantantai.3013@gmail.com>
2025-11-29 04:50:26 +0700

    test logout

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -162,19 +162,9 @@ class AdminController extends Controller {
     }
 
     public function table() {
-        // require_once __DIR__ . '/TableController.php';
-        // $tableController = new TableController();
-        // $tableController->adminIndex();
-         // Load tables
-        // $tableModel = $this->model('Table');
-        // $tables = $tableModel->getAllTables();
-        
-        $data = [
-            'title' => 'Quản lý bàn'
-            // 'tables' => $tables
-        ];
-        // Pass data to the view
-        $this->view('admin/home/index', $data);
+        require_once __DIR__ . '/TableController.php';
+        $tableController = new TableController();
+        $tableController->adminIndex();
     }
 
 }
--- app/controllers/TableController.php
+++ app/controllers/TableController.php
@@ -1,16 +1,35 @@
 <?php
 class TableController extends Controller {
+
+    private function checkAuth() {
+        if (!isset($_SESSION['user']) || empty($_SESSION['user']['ID'])) {
+            echo "<script>
+                alert('Vui lòng đăng nhập để truy cập!');
+                window.location.href = '" . BASE_URL . "login_admin';
+            </script>";
+            exit();
+        }
+
+        $role = $_SESSION['user']['Role'];
+        if (!in_array($role, [0, 1, 2, 3])) {
+            echo "<script>
+                alert('Bạn không có quyền truy cập trang này!');
+                window.location.href = '" . BASE_URL . "login_admin';
+            </script>";
+            exit();
+        }
+    }
     
     public function adminIndex() {
         $this->checkAuth();
         
         // Load tables
-        // $tableModel = $this->model('Table');
-        // $tables = $tableModel->getAllTables();
+        $tableModel = $this->model('Table');
+        $tables = $tableModel->getAllTables();
         
         $data = [
-            'title' => 'Quản lý bàn'
-            // 'tables' => $tables
+            'title' => 'Quản lý bàn',
+            'tables' => $tables
         ];
         // Pass data to the view
         $this->view('admin/home/index', $data);
