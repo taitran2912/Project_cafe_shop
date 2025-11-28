9f947f2b9b872b95cf7ccaa9f8b51c7f8e6b93d7
taitran2912 <trantantai.3013@gmail.com>
2025-11-29 05:57:10 +0700

    test logout

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -6,7 +6,7 @@
     <title><?= $data['title'] ?></title>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="public/css/login.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>public/css/login.css">
 </head>
 <body>
     <div class="login-container">
