42c2cc32757324feb93841f53ef7bba6776cc388
taitran2912 <trantantai.3013@gmail.com>
2025-11-29 00:57:28 +0700

    test logout

--- app/views/layout/headerAdmin.php
+++ app/views/layout/headerAdmin.php
@@ -14,14 +14,12 @@
         <!-- Notification -->
         <button class="btn-notification text-gray-700 text-xl relative">
             <i class="fas fa-bell"></i>
-            <span class="badge absolute -top-1 -right-1 bg-red-500 text-white text-xs px-1.5 rounded-full">3</span>
+            <!-- <span class="badge absolute -top-1 -right-1 bg-red-500 text-white text-xs px-1.5 rounded-full">3</span> -->
         </button>
 
         <!-- Logout -->
-        <a href="https://caffeshop.hieuthuocyentam.id.vn/logout"
-           class="flex items-center gap-2 px-3 py-1.5 bg-red-500 text-dark text-sm rounded-lg hover:bg-red-600 transition">
+        <a href="https://caffeshop.hieuthuocyentam.id.vn/logout" class="btn-notification text-gray-700 text-xl relative">
             <i class="fas fa-sign-out-alt"></i>
-            Đăng xuất
         </a>
     </div>
 </header>
