6735a6860bb193b0d1a809e8171c0bb21584d961
taitran2912 <trantantai.3013@gmail.com>
2025-11-29 01:00:13 +0700

    test logout

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends Controller {
         }
 
         $role = $_SESSION['user']['Role'];
-        if (!in_array($role, [1, 2, 3])) {
+        if (!in_array($role, [0, 1, 2, 3])) {
             echo "<script>
                 alert('Bạn không có quyền truy cập trang này!');
                 window.location.href = '" . BASE_URL . "login_admin';
