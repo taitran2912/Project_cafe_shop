736439921c0e5e0c8b9a0118c99ebacf00e4ceb4
taitran2912 <trantantai.3013@gmail.com>
2025-11-29 05:56:29 +0700

    test logout

--- app/views/admin/login/index.php
+++ app/views/admin/login/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng Nhập - Quán Cafe</title>
-    <link rel="stylesheet" href="public/css/LoginAdmin.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>public/css/LoginAdmin.css">
 </head>
 <body>
     <div class="login-container">
