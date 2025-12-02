a4ab8047460da8dbb95c40b13b4550bb80850004
taitran2912 <trantantai.3013@gmail.com>
2025-12-02 23:18:53 +0700

    test logout

--- app/views/checkout/index.php
+++ app/views/checkout/index.php
@@ -172,13 +172,14 @@
                 data: { orderID: orderID },
                 success: function(response) {
                     // Response mong muốn: { status: "Paid" } hoặc { status: "Pending" }
-                    if (response.status === "Paid") {
-                        // Dừng kiểm tra
-                        clearInterval(checkInterval);
+                    // if (response.status === "Paid") {
+                    //     // Dừng kiểm tra
+                    //     clearInterval(checkInterval);
 
-                        // Chuyển sang trang cảm ơn
-                        window.location.href = "https://caffeshop.hieuthuocyentam.id.vn/payment/thankyou/" + orderID;
-                    }
+                    //     // Chuyển sang trang cảm ơn
+                    //     window.location.href = "https://caffeshop.hieuthuocyentam.id.vn/payment/thankyou/" + orderID;
+                    // }
+                    conslole.log(response);
                 },
                 error: function(xhr) {
                     console.error("Lỗi khi kiểm tra trạng thái đơn:", xhr.responseText);
