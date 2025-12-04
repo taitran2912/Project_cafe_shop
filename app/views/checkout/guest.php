ed431a6ab6d9e46f481e0405a15905f7d70d3358
taitran2912 <trantantai.3013@gmail.com>
2025-12-05 01:04:05 +0700

    test logout

--- app/views/checkout/guest.php
+++ app/views/checkout/guest.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout - Đặt Hàng Cà Phê</title>
-    <link rel="stylesheet" href="https://caffeshop.hieuthuocyentam.id.vn/public.css/style.css">
+    <link rel="stylesheet" href="https://caffeshop.hieuthuocyentam.id.vn/public/css/style.css">
 </head>
 <body>
     <div class="container">
