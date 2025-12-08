96fe6008d20c1beb3351069739715b027b3ed9d1
taitran2912 <trantantai.3013@gmail.com>
2025-12-09 06:28:48 +0700

    test logout

--- public/js/profile.js
+++ public/js/profile.js
@@ -167,6 +167,10 @@ function loadAddresses() {
 
     container.innerHTML = `
         <h2 class="font-display text-2xl font-bold mb-6">Địa chỉ nhận hàng</h2>
+        <button onclick="openAddressModal()" 
+        class="px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary/90">
+        + Thêm địa chỉ
+    </button>
         ${html}
     `;
 }
