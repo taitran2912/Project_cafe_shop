d6f0d3147d78bdbca04028f736473ca397a7cef0
taitran2912 <trantantai.3013@gmail.com>
2025-12-09 04:46:39 +0700

    test logout

--- app/views/checkout/guest.php
+++ app/views/checkout/guest.php
@@ -260,7 +260,7 @@ function confirmOrder() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(order)
     })
-    ..then(r => r.json())
+    .then(r => r.json())
     .then(res => {
         console.log("SERVER RESPONSE:", res); // <=== THÊM DÒNG NÀY
 
